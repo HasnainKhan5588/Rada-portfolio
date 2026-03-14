96eda9f14980544e095ba519901c6330435ebce1
Muhammad Husnain <muhammadhusnainafghan@gmail.com>
2026-03-14 10:13:15 +0500

    some changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,7 +15,6 @@ function Contact() {
       </h1>
 
       <div className="flex flex-wrap justify-center md:justify-between gap-10 w-full max-w-6xl">
-      
         <div className="bg-gray-900 rounded-2xl shadow-xl p-10 flex flex-col items-center w-full md:w-[40%] hover:scale-105 transform transition-all duration-300">
           <h2 className="text-2xl md:text-3xl font-bold text-white mb-6">
             Send Me a Message
@@ -32,14 +31,13 @@ function Contact() {
           </button>
         </div>
 
-      
         <div className="bg-gray-900 rounded-2xl shadow-xl p-10 w-full md:w-[40%] flex flex-col gap-4 hover:scale-105 transform transition-all duration-300">
           <h2 className="font-bold text-2xl md:text-3xl text-white mb-4">
             Get in Touch
           </h2>
           <p className="text-gray-300 cursor-pointer hover:text-blue-500 flex items-center transition-all">
             <span className="hidden sm:flex">📧</span>
-            muhammadhusnainafghan@gmail.com
+            rada629015@gmail.com
           </p>
           <p className="text-gray-300">📞 +92 3379867539</p>
           <p className="text-gray-300">📍 Mardan, Pakistan</p>
